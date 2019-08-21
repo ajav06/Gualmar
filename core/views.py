b9f25470ad3c1cf2f73fc8b8fe8a012f48d31949
Anyi Villarreal <anyeliv02@gmail.com>
2019-08-21 14:46:51 -0400

    ajax done

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.http import JsonResponse
+from .models import Article
 
 from . import models
 
@@ -13,3 +15,15 @@ class DashboardViews(ListView):
         context = super().get_context_data(**kwargs)
         context['categories'] = models.CategoryArticle.objects.all()
         return context
+
+def obtenerarticulo(request):
+    codigo = request.POST.get('codigo', None)
+    articulo = Article.objects.get(code=codigo)
+    data = {
+        'nombre' : articulo.name,
+        'descripcion' : articulo.descripcion,
+        'precio' : articulo.price,
+        'categoria' : articulo.categories,
+        'image' : articulo.image.url,
+    }
+    return JsonResponse(data)
--- gualmar/settings.py
+++ gualmar/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'agents.apps.AgentsConfig',
     'core.apps.CoreConfig',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
