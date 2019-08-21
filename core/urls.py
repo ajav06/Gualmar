a21a3b483897dde3bd1e733d3ab97a9b20e2f050
Anyi Villarreal <anyeliv02@gmail.com>
2019-08-21 13:38:53 -0400

    carrito

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='core/login.html')),
     path('dashboard/', TemplateView.as_view(template_name='core/dashboard.html'), name="dashboard"),
+    path('cart/', TemplateView.as_view(template_name='core/cart.html'), name="cart"),
 ]
