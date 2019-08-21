94aca3aa2ab26d2399eea81c62bd428bfb79e566
ajav06 <albert06av@hotmail.com>
2019-08-21 01:11:22 -0400

    models v2

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from core.models import User, Address
+from core.models import User, Address, CategoryArticle, Article, Search
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
@@ -10,5 +10,25 @@ class AddressAdmin(admin.ModelAdmin):
     list_display = ('user', 'country', 'location', 'city', 'address')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('name', 'created', 'updated')
+
+class ArticleAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', )
+    list_display = ('code', 'name', 'description', 'price', 'post_categories')
+
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by('name')])
+    post_categories.short_description = 'Categorías'
+
+class SearchAdmin(admin.ModelAdmin):
+#    readonly_fields = ('id_session', 'user', 'time')
+    list_display = ('id_session', 'user', 'phrase', 'time')
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Address, AddressAdmin)
+admin.site.register(CategoryArticle, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Search, SearchAdmin)
--- core/models.py
+++ core/models.py
@@ -28,4 +28,90 @@ class Address(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
+
+class CategoryArticle(models.Model):
+    name = models.CharField(verbose_name='Nombre', max_length=100)
+    created = models.DateTimeField(verbose_name='Fecha de Creación', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Fecha de Edición', auto_now=True)
+
+    class Meta:
+        verbose_name = 'categoría de artículo'
+        verbose_name_plural = 'categorías de artículos'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    code = models.CharField(max_length=10, primary_key=True, verbose_name="Código")
+    name = models.CharField(max_length=50, unique=True, verbose_name="Nombre")
+    description = models.TextField(verbose_name="Descripción")
+    price = models.FloatField(verbose_name="Precio", max_length=30)
+    categories = models.ManyToManyField(CategoryArticle, verbose_name='Categorías', related_name='get_posts')
+    created = models.DateTimeField(verbose_name='Fecha de Creación', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'artículo'
+        verbose_name_plural = 'artículos'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Search(models.Model):
+    id_session = models.CharField(max_length=50, verbose_name="Id de la Sesión")
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE)
+    phrase = models.CharField(max_length=50, verbose_name="Frase de Búsqueda")
+    time = models.FloatField(max_length=10, verbose_name="Tiempo de Búsqueda")
+
+    class Meta:
+        verbose_name = 'búsqueda de artículo'
+        verbose_name_plural = 'búsqueda de artículos'
+
+    def __str__(self):
+        return self.phrase
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE)
+    amount = models.FloatField(max_length=30, verbose_name="Monto de Compra")
+    address = models.ForeignKey(Address, verbose_name="Dirección de Envio", on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now, verbose_name="Fecha de Compra")
+
+    class Meta:
+        verbose_name = 'compra'
+        verbose_name_plural = 'compras'
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'carrito de compra'
+        verbose_name_plural = 'carritos de compra'
+
+
+class PaymentDetails(models.Model):
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name = 'detalles de pago'
+        verbose_name_plural = 'detalles de pagos'
+
+class Bill(models.Model):
+    user = models.ForeignKey(User, verbose_name="Usuario", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'factura'
+        verbose_name_plural = 'facturas'
+
+
+class BillDetails(models.Model):
+    bill = models.ForeignKey(Bill, verbose_name="Id de la Factura", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'detalle de factura'
+        verbose_name_plural = 'detalles de facturas'
