1b9b3e6c123e1efb0dd9faa7a85c77ba7717c70b
Anyi V <anyeliv02@gmail.com>
2019-08-21 17:47:52 -0400

    otra vez u.u

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,8 @@ urlpatterns = [
     path('', views.login.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('dashboard/', views.DashboardViews.as_view(), name="dashboard"),
-<<<<<<< HEAD
     path('cart/', TemplateView.as_view(template_name='core/cart.html'), name="cart"),
     path('profile/', TemplateView.as_view(template_name='core/profile.html'), name="profile"),
-    path('ajax/getarticle/', views.obtenerarticulo, name='obtenerarticulo'),
-=======
     path('cart/<pk>/', views.ListShoppingCart.as_view(), name="cart"),
->>>>>>> e1730f53e576d368220d66089d28b3ebfa8b61c5
     path('dashboard/ajax/getarticle/', views.obtenerarticulo, name='obtenerarticulo'),
 ]
