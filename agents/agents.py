eb24f2456edb33d3463e7d537297041a02eb1344
Anyi Villarreal <anyeliv02@gmail.com>
2019-08-23 02:04:33 -0400

    agentes

--- agents/agents.py
+++ agents/agents.py
@@ -4,32 +4,41 @@ from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.message import Message
 from spade.template import Template
-from core.models import Search
+from core.models import Search, User
 
-class AgenteEscucha(Agent):
-    ult_busqueda = Search.objects.all().order_by('id')
-    print(ult_busqueda)
+class AgenteGualmar(Agent): ##El Agente
+    ult_busqueda = Search.objects.all().order_by('-id')[0] ##Guarda la última búsqueda
+    ## al momento de iniciar el servidor
+    tabla_interes = [] ##Instancia una tabla de interés, vacía, que luego llenará con datos de cada usuario
+
+    class ConstruirTablaInteresPorUsuario(OneShotBehaviour): ##Comportamiento que construye la tabla
+        async def run(self):
+            for usuario in User.objects.all(): ##Por cada usuario, realiza lo siguiente:
+                keywords = dict() ##Crea un diccionario que guardará sus keywords
+                busquedas = Search.objects.filter(user_id=usuario.id) ##1) Obtiene todas sus búsquedas,
+                for busqueda in busquedas: ##2) Iterando sobre todas sus búsquedas,
+                    palabras = busqueda.phrase.split() ##consigo las frases, las divido en palabras.
+                    for palabra in palabras: ##3) Por cada palabra,
+                        if not keywords[palabra]: ##Si la palabra no existe en los keywords, la añado
+                            keywords[palabra] = 1
+                        else: ##Si la palabra existe en los keywords, le incremento su interés.
+                            keywords[palabra] += 1
+                ##Con esto basta para tener todas las keywords que ha buscado un usuario.
+                interes_usuario = { ##Ahora, construyo su elemento para el interes
+                    'usuario':usuario.id,
+                    'ult_conexion':usuario.last_login,
+                    'keywords':[
+                        keywords
+                    ]
+                }
+                self.tabla_interes.append(interes_usuario) ##Añado a la tabla de interés.
+            print(self.tabla_interes)
 
     async def setup(self):
         print("Comenzando el agente {}".format(str(self.jid)))
         self.b = self.EnviarMensaje()
         self.add_behaviour(self.b)
 
-class AgenteRecibe(Agent):
-    class RecibirMensaje(OneShotBehaviour):
-        async def run(self):
-            msg = await self.receive(timeout=10)
-            if msg:
-                print("{} dice: Recibí un mensaje con el contenido: '{}'".format(self.agent.jid,msg.body))
-            else:
-                print("No he recibido mensajes en los últimos 10 segundos.")
-            self.kill
-
-    async def setup(self):
-        print("Comenzando el agente {}".format(str(self.jid)))
-        self.b = self.RecibirMensaje()
-        self.add_behaviour(self.b)
-
-enviador = AgenteEnvia("listener_agent@404.city","123456")
-receptor = AgenteRecibe("receiver_agent@404.city","123456")
-dummy = AgentePrueba("dummy_agent@404.city", "123456")
+def ArrancarAgentes():   
+    agente = AgenteGualmar("listener_agent@404.city","123456")
+    agente.start()
--- gualmar/urls.py
+++ gualmar/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from agents.agents import ArrancarAgentes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,5 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+ArrancarAgentes()
